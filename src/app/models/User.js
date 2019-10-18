903ddfe86e99320442ca5c0228b1e936e7be84a7
burn-c <chopcsom@gmail.com>
2019-10-18 19:53:04 -0300

    The one

---
+++ src/app/models/User.js
@@ -0,0 +1,31 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // Campo Virtual só existe no lado do código
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+    },
+    {
+      sequelize,
+    }
+    );
+
+    // Este Hook será executado antes de ir para o banco de dados
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+
+    });
+
+    return this;
+  }
+}
+
+export default User;
