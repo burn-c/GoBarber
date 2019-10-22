a6724338f43b5f9e41553d899dd761c704eab699
burn-c <chopcsom@gmail.com>
2019-10-22 19:58:38 -0300

    Feature: Notification->Read

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -21,7 +21,18 @@ class NotificationController {
         .sort({createdAt: 'desc'})
         .limit(20);
         return res.json(notifications);
-    }
+
+        }
+
+        async update(req,res) {
+            const notification = await Notification.findByIdAndUpdate(
+                req.params.id,
+                { read: true },
+                { new: true },
+            );
+
+            return res.json(notification);
+        }
 }
 
 export default new NotificationController();
