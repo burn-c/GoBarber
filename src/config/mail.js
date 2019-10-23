2f31a0ab64ace0efbeca79c933134635d4680422
burn-c <chopcsom@gmail.com>
2019-10-22 21:50:10 -0300

    Feature: Appointment->Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0bbb9b6c60cf2d',
+    pass: '7c97fcb8ad6d7f'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply#gobarber.com>'
+  }
+};
